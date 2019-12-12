88d643456c53b876379d045ce8a1bffcae2d1782
SulJis <mx.slipchenko@gmail.com>
2019-12-13 01:51:56 +0200

    deploy problems

---
+++ build/precache-manifest.0ec3318f8087dbf09cf4b6c040f2b994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c624289c858d154328eba1c7a2fa7c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecbf0c752c4541c99f3",
+    "url": "/static/css/main.5235e588.chunk.css"
+  },
+  {
+    "revision": "77db5bb82153d9c3eaa1",
+    "url": "/static/js/2.a1a5ff16.chunk.js"
+  },
+  {
+    "revision": "0ecbf0c752c4541c99f3",
+    "url": "/static/js/main.8fce8c4a.chunk.js"
+  },
+  {
+    "revision": "5b1261aad88b6f86d425",
+    "url": "/static/js/runtime-main.c3e9595b.js"
+  }
+]);
